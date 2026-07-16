a232c0925d51dec374d4c1327b6e20f3b0a6687b
Mltiformacionjose <multiformacionjose@gmail.com>
2026-07-16 11:51:56 +0100

    Fix: Javascript path

--- components/footer/footerInjection.js
+++ components/footer/footerInjection.js
@@ -1,5 +1,5 @@
 fetch("components/footer/componentFooter.html")
-then(response => response.text())
+.then(response => response.text())
 .then(data => {
     document.getElementById("footerInjection").innerHTML = data;
 });
