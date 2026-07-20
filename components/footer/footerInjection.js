b70b7bbee2482b52daa2c78b2f3ae85937dce98f
carlosjper <cjavierp500@gmail.com>
2026-07-20 13:56:56 +0200

    feat: changes in groups branch

--- components/footer/footerInjection.js
+++ components/footer/footerInjection.js
@@ -1,5 +1,7 @@
-fetch("components/footer/componentFooter.html")
+fetch("../componentFooter.html")
 .then(response => response.text())
 .then(data => {
     document.getElementById("footerInjection").innerHTML = data;
 });
+
+// fetch("components/footer/componentFooter.html")
